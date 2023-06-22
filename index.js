c5ef930677082606db41a6bd51303cabe44c97ab
victor mak <thegreentechoffice@gmail.com>
2023-06-22 10:29:07 +0100

    Updated app

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ app.get("/", (req, res) => {
    res.send("Hello Server.js")
 });
 
-app.get("/checker", async(req, res) => {
+app.get("/checker", (req, res) => {
   let options = {};
 
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+(async () => {
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
-
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+        };
+    }
     try {
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
@@ -37,8 +37,8 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     await page.locator("#old-password").fill("tesjhjashjhajsasjk")
     await page.locator('body > div.db-Login-root.db-Login-root--v4 > div > div > div.db-RegisterAndLoginLayout.db-RegisterAndLoginLayout--login.db-RegisterAndLoginLayout--isMobile.Box-root.Flex-flex.Flex-direction--column > div.db-RegisterAndLoginLayout-contentWrapper.Box-root > div > div.Card-root.Card--radius--all.Card--shadow--large.db-RegisterAndLoginLayout-card.Box-root.Box-hideIfEmpty.Box-background--white > div > div > div > div > div > div > div:nth-child(2) > form > div > div > div > div:nth-child(4) > div > div > div > div > div > div.PressableCore-base.Box-root > button').click()
     await page.waitForSelector('.Text-color--red', {timeout: 3000}).then(() => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({msg: 'Incorrect email or password'}));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({msg: 'Incorrect email or password'}));
     }).catch(() => {
     res.send(JSON.stringify({msg: 'Success'}))
     })
@@ -47,8 +47,8 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
         console.error(error);
         return null;
     }
- 
 })();
+})
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`)
