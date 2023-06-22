99e11226937624a3201078d4c7a805f5db86f9e3
victor mak <thegreentechoffice@gmail.com>
2023-06-22 11:28:38 +0100

    Updated app

--- index.js
+++ index.js
@@ -25,23 +25,23 @@ let options = {};
         args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
         defaultViewport: chrome.defaultViewport,
         executablePath: await chrome.executablePath,
-        headless: false,
+        headless: true,
         ignoreHTTPSErrors: true,
         };
     }
     try {
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
-    await page.goto('https://dashboard.stripe.com/login');
-    await page.locator("#email").fill("test@mails.com")
-    await page.locator("#old-password").fill("tesjhjashjhajsasjk")
-    await page.locator('body > div.db-Login-root.db-Login-root--v4 > div > div > div.db-RegisterAndLoginLayout.db-RegisterAndLoginLayout--login.db-RegisterAndLoginLayout--isMobile.Box-root.Flex-flex.Flex-direction--column > div.db-RegisterAndLoginLayout-contentWrapper.Box-root > div > div.Card-root.Card--radius--all.Card--shadow--large.db-RegisterAndLoginLayout-card.Box-root.Box-hideIfEmpty.Box-background--white > div > div > div > div > div > div > div:nth-child(2) > form > div > div > div > div:nth-child(4) > div > div > div > div > div > div.PressableCore-base.Box-root > button').click()
-    await page.waitForSelector('.Text-color--red', {timeout: 3000}).then(() => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({msg: 'Incorrect email or password'}));
-    }).catch(() => {
-    res.send(JSON.stringify({msg: 'Success'}))
-    })
+    await page.goto('https://google.com');
+    // await page.locator("#email").fill("test@mails.com")
+    // await page.locator("#old-password").fill("tesjhjashjhajsasjk")
+    // await page.locator('body > div.db-Login-root.db-Login-root--v4 > div > div > div.db-RegisterAndLoginLayout.db-RegisterAndLoginLayout--login.db-RegisterAndLoginLayout--isMobile.Box-root.Flex-flex.Flex-direction--column > div.db-RegisterAndLoginLayout-contentWrapper.Box-root > div > div.Card-root.Card--radius--all.Card--shadow--large.db-RegisterAndLoginLayout-card.Box-root.Box-hideIfEmpty.Box-background--white > div > div > div > div > div > div > div:nth-child(2) > form > div > div > div > div:nth-child(4) > div > div > div > div > div > div.PressableCore-base.Box-root > button').click()
+    // await page.waitForSelector('.Text-color--red', {timeout: 3000}).then(() => {
+    // res.setHeader('Content-Type', 'application/json');
+    // res.send(JSON.stringify({msg: 'Incorrect email or password'}));
+    // }).catch(() => {
+    // res.send(JSON.stringify({msg: 'Success'}))
+    // })
     await page.close();
     } 
     catch (error) {
